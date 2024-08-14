4777b0a4861e7744943c3edc3cca11f957c53db7
Ellowen <lrubio@sacoa.com>
2024-08-14 15:24:27 -0300

    va

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Landing_MFB/', // Reemplaza {nombre-del-repositorio} por el nombre de tu repositorio
+});
