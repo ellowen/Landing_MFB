beac43fc29cbb9f4f5e753757fca2542fb8557d6
Lucas Rubio <lrubio@sacoa.com>
2024-08-15 14:19:11 -0300

    uno mas

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: './', // Reemplaza {nombre-del-repositorio} por el nombre de tu repositorio
+  build: {
+    outDir: 'dist',
+  },
 });
