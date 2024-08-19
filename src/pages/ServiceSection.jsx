8faac53ec1603bdc2e9bbb35035785dc4ffb4292
Ellowen <lrubio@sacoa.com>
2024-08-19 10:18:33 -0300

    deploy

--- src/pages/ServiceSection.jsx
+++ src/pages/ServiceSection.jsx
@@ -5,6 +5,12 @@ const ServiceSection = () => {
   const sectionRef = useRef(null);
 
   useEffect(() => {
+    const isMobile = /Mobi|Android/i.test(navigator.userAgent);
+    if (isMobile) {
+      setLoadVideos(true);
+      return; // No uses el observer en móviles
+    }
+
     const observer = new IntersectionObserver(
       (entries) => {
         const entry = entries[0];
@@ -31,17 +37,58 @@ const ServiceSection = () => {
     };
   }, []);
 
+
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver(
+  //     (entries) => {
+  //       const entry = entries[0];
+  //       if (entry.isIntersecting) {
+  //         setLoadVideos(true);
+  //         observer.disconnect(); // Detener la observación después de que se carguen los videos
+  //       }
+  //     },
+  //     {
+  //       root: null, // viewport
+  //       rootMargin: '0px',
+  //       threshold: 0.1, // 10% del componente debe ser visible para activar la carga
+  //     }
+  //   );
+
+  //   if (sectionRef.current) {
+  //     observer.observe(sectionRef.current);
+  //   }
+
+  //   return () => {
+  //     if (observer && sectionRef.current) {
+  //       observer.unobserve(sectionRef.current);
+  //     }
+  //   };
+  // }, []);
+
   const renderYouTubeIframe = (videoId) => (
     <iframe
       width="100%"
-      height="250"
-      src={`https://www.youtube.com/embed/${videoId}?autoplay=0`}
+      height="auto"
+      style={{ aspectRatio: '16/9' }} // Mantén la relación de aspecto 16:9
+      src={`https://www.youtube.com/embed/${videoId}?autoplay=0&mute=1`}
       title="YouTube video player"
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
     ></iframe>
   );
+
+  // const renderYouTubeIframe = (videoId) => (
+  //   <iframe
+  //     width="100%"
+  //     height="250"
+  //     src={`https://www.youtube.com/embed/${videoId}?autoplay=0`}
+  //     title="YouTube video player"
+  //     frameBorder="0"
+  //     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+  //     allowFullScreen
+  //   ></iframe>
+  // );
 
   return (
     <section id="services" className="services section dark-background" ref={sectionRef}>
@@ -57,6 +104,7 @@ const ServiceSection = () => {
         <div className="row gy-4">
           <div className="col-lg-4 col-md-6" data-aos="fade-up" data-aos-delay={100}>
             <div className="service-item position-relative">
+              {/* {loadVideos && renderYouTubeIframe("I6ApqvOQEDQ")} */}
               {loadVideos && renderYouTubeIframe("I6ApqvOQEDQ")}
             </div>
           </div>
--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,23 @@ export default defineConfig({
   build: {
     outDir: 'dist',
   },
+  server: {
+    host: '0.0.0.0',
+    port: 3055, 
+    open: true,
+    hmr: {
+      host: '127.0.0.1', 
+      port: 443, 
+    },
+   
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:33000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+
 });
